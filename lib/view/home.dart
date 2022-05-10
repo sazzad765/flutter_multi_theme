417f6c701a6ee199f355aae0d91f34cf3eef5afa
sazzad765 <shfahad.920@gmail.com>
2022-05-10 23:41:35 +0600

    theme update

--- lib/view/home.dart
+++ lib/view/home.dart
@@ -17,23 +17,46 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     final theme = Provider.of<ThemeNotifier>(context);
+    final _size = MediaQuery.of(context).size;
     return Scaffold(
       appBar: AppBar(
         title: const Text('Hybrid Theme'),
-        actions: [Switch(value: theme.isDark, onChanged: (value) {
-          if(!value){
-            theme.setLightMode();
-          }else{
-            theme.setDarkMode();
-          }
-        },)],
+        actions: [
+          Switch(
+            value: theme.isDark,
+            onChanged: (value) {
+              if (!value) {
+                theme.setLightMode();
+              } else {
+                theme.setDarkMode();
+              }
+            },
+          )
+        ],
       ),
-      body: Center(child: Column(children: [
-        ElevatedButton(onPressed: () {
-          Navigator.pushNamed(context, RouteName.generateScreen);
-
-        }, child: Text('Ge'))
-      ],),),
+      body: Container(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            ElevatedButton(
+                onPressed: () {
+                  Navigator.pushNamed(context, RouteName.generateScreen);
+                },
+                child: Text('Ge')),
+            Container(
+              decoration: const BoxDecoration(
+                borderRadius:
+                    BorderRadius.only(bottomLeft: Radius.circular(20.0)),
+                color: CustomColors.primary,
+              ),
+              height: 100,
+              // width: ?,
+              child: const Text('Scan'),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
